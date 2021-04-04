b52670763e76b2fa8c052b396e33da4d8d56ff38
AMINE <amineelkahza@gmail.com>
2021-04-05 00:37:01 +0200

    one toone relation ship

---
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -0,0 +1,28 @@
+package guru.springframework.domain;
+import javax.persistence.*;
+/**
+ * Created by Lenovo on 05/04/2021.
+ */
+@Entity
+public class UnitOfMeasure {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
